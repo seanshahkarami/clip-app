05db182f3fd3864833ce3b2896c8db49f2e4b290
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-10-26 16:42:22 -0500

    skip inference step during download models

--- download-models.py
+++ download-models.py
@@ -1,11 +1,4 @@
 from transformers import CLIPProcessor, CLIPModel
-import torch
 
-# automatically downloads model data
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-
-# do single pass on dummy data
-inputs = processor(text=["dummy"], images=torch.zeros((3, 128, 128), dtype=torch.uint8), return_tensors="pt", padding=True)
-with torch.no_grad():
-    outputs = model(**inputs)
