34a24eae103c21b99bde28d258c27dd0621a0ead
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-10-26 16:31:54 -0500

    using new base image

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM python:3.10
+FROM ecpe4s/waggle-ml
 
 WORKDIR /app
 
 COPY requirements.txt .
-RUN pip3 install --upgrade --no-cache-dir -r requirements.txt && \
-    pip3 install --force-reinstall opencv-python-headless
+RUN pip3 install --no-cache-dir -r requirements.txt && \
+    pip3 install --no-cache-dir --force-reinstall opencv-python-headless
 #   ^ this is a hack to replace the "gui" version of opencv pulled in as a dep with headless again
 
 COPY download-models.py .
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
 import torch
-
 from transformers import CLIPProcessor, CLIPModel
 from waggle.plugin import Plugin
 from waggle.data.vision import Camera
@@ -22,28 +21,27 @@ def main():
     processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
     model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 
-    with Plugin() as plugin, Camera(args.input) as camera:
-        for snapshot in camera.stream():
-            inputs = processor(text=args.text, images=snapshot.data, return_tensors="pt", padding=True)
-            with torch.no_grad():
-                outputs = model(**inputs)
-            logits_per_image = outputs.logits_per_image # this is the image-text similarity score
-            probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
+    camera = Camera(args.input)
 
-            for prob, logits, description in sorted(zip(probs.view(-1), logits_per_image.view(-1), args.text)):
-                # TODO prefer similarity score to softmax prob for thresholding. software prob can give unituitive
-                # results - for example, when a single text is provided, that will always be published.
-                matched = (
-                    (args.threshold_type == "similarity" and logits > args.threshold) or
-                    (args.threshold_type == "softmax" and prob > args.threshold)
-                )
+    while True:
+        snapshot = camera.snapshot()
+        inputs = processor(text=args.text, images=snapshot.data, return_tensors="pt", padding=True)
+        with torch.no_grad():
+            outputs = model(**inputs)
+        logits_per_image = outputs.logits_per_image # this is the image-text similarity score
+        probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
 
-                if matched:
-                    plugin.publish("image.clip.prediction", f"{description},{logits:0.3f},{prob:0.3f}", timestamp=snapshot.timestamp)
-                
-                marker = "*" if matched else " "
-                print(f"{logits:0.3f} {prob:0.3f} {marker} {description} ")
-            print(flush=True)
+        for prob, logits, description in sorted(zip(probs.view(-1), logits_per_image.view(-1), args.text)):
+            # TODO prefer similarity score to softmax prob for thresholding. software prob can give unituitive
+            # results - for example, when a single text is provided, that will always be published.
+            matched = (
+                (args.threshold_type == "similarity" and logits > args.threshold) or
+                (args.threshold_type == "softmax" and prob > args.threshold)
+            )
+            
+            marker = "*" if matched else " "
+            print(f"{logits:0.3f} {prob:0.3f} {marker} {description} ")
+        print(flush=True)
 
 
 if __name__ == "__main__":
